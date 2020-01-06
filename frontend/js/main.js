8bf0e78ee954c18ccbb5e9f7929496fe42e9dc51
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-01-06 01:54:45 +0100

    added progress bar. bug with exit-btn

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,7 +2,7 @@
 // Configuration variables
 const CONFIG = {
     "APIUrl" : "https://opentdb.com/api.php",
-    "secondsPerQuestion" : 6,
+    "secondsPerQuestion" : 30,
     "colors" : {
         "green" : "#0c0",
         "red" : "#f22",
@@ -25,7 +25,9 @@ const questionContainer = document.getElementById("questions-container");
 const questionNumber = document.getElementById("question-number");
 const questionText = document.getElementById("question-text");
 const answersContainer = document.getElementById("answers");
+const exitBtn = document.getElementById("exit-btn");
 const timer = document.getElementById("timer");
+const progressBar = document.getElementById("progress-bar");
 const resultsSection = document.getElementById("results-section");
 const resultsSummary = document.getElementById("results-summary");
 const resultsCorrects = document.getElementById("results-corrects");
@@ -81,11 +83,6 @@ class Form
 
 class Game
 {
-    // Stores configurations for the application
-    static configurations = {
-
-    }
-
     // Stores data about the game to keep track of the state
     static gameData = {
         "stats" : {
@@ -100,12 +97,25 @@ class Game
         }
     };
 
-    static init(questions) {
-        Game.resetGameData(questions);
-        Game.startGame(questions);
+    static init(APIData) {
+        // If there aren't as many questions as requested
+        if (APIData.response_code == 1) {
+
+        }
+        // Different invalid (internal) errors
+        if (APIData.response_code >= 2) {
+
+        }
+        // If everything went OK
+        if (APIData.response_code == 0) {
+            const questions = APIData.results;
+            Game.initializeGameData(questions);
+            Game.startGame(questions);
+        }
+        
     }
 
-    static resetGameData(questions) {
+    static initializeGameData(questions) {
         Game.gameData.stats["correct"] = 0;
         Game.gameData.stats["wrong"] = 0;
         Game.gameData.questions = questions;
@@ -113,10 +123,19 @@ class Game
         Game.gameData.nrOfQuestionsAnswered = 0;
     }
 
+    static resetGameData() {
+        Game.gameData.stats["correct"] = 0;
+        Game.gameData.stats["wrong"] = 0;
+        Game.gameData.questions = [];
+        Game.gameData.nrOfQuestions = 0;
+        Game.gameData.nrOfQuestionsAnswered = 0;
+    }
+
     static startGame(questions) {
         Form.hideFormSection();
         GameUI.showQuestionSection();
         Game.startQuestion(questions[0]);
+        GameUI.createProgressBar(Game.gameData.nrOfQuestions);
     }
 
     static startQuestion(question) {
@@ -143,16 +162,18 @@ class Game
         answersContainer.addEventListener("click", e => {
             if (e.target.classList.contains("answer")) {
                 if (hasAnswered == false) {
-                    const userChoice = e.target.innerText;
+                    const userChoice = e.target.innerHTML;
                     if (userChoice == question["correct_answer"]) {
                         e.target.style.border = `2px solid ${CONFIG.colors.green}`;
                         e.target.style.backgroundColor = `${CONFIG.colors.green}`;
                         Game.gameData.stats.correct++;
+                        GameUI.updateProgressBar(Game.gameData.nrOfQuestionsAnswered, "correct");
                     }
                     else {
                         e.target.style.border = `2px solid ${CONFIG.colors.red}`;
                         e.target.style.backgroundColor = `${CONFIG.colors.red}`;
                         Game.gameData.stats.wrong++;
+                        GameUI.updateProgressBar(Game.gameData.nrOfQuestionsAnswered, "wrong");
                     }
                     clearInterval(refreshId);
                     hasAnswered = true;
@@ -168,8 +189,8 @@ class Game
     }
 
     static showQuestion(question) {
-        questionNumber.innerText = `Question ${Game.gameData.nrOfQuestionsAnswered + 1}`;
-        questionText.innerText = question["question"]; 
+        questionNumber.innerHTML = `Question ${Game.gameData.nrOfQuestionsAnswered + 1}`;
+        questionText.innerHTML = question["question"]; 
     }
 
     static getAlternativeAnswers(question) {
@@ -185,7 +206,7 @@ class Game
 
     static showAlternativeAnswers(answers) {
         answersContainer.innerHTML = "";
-        //console.log(answers);
+
         answers.forEach(answer => {
             const answerDiv = document.createElement("div");
             answerDiv.appendChild(document.createTextNode(answer));
@@ -199,7 +220,7 @@ class Game
         Game.gameData.timer.timeRemaining = CONFIG.secondsPerQuestion;
 
         // let currentTime = SECONDS_PER_QUESTION;
-        timer.innerText = Game.gameData.timer.timeRemaining;
+        timer.innerHTML = Game.gameData.timer.timeRemaining;
 
         const refreshId = setInterval(() => {
             Game.gameData.timer.timeRemaining--;
@@ -209,7 +230,7 @@ class Game
             if (Game.gameData.timer.timeRemaining == 5) {
                 GameUI.makeTimerRed();
             }
-            timer.innerText = Game.gameData.timer.timeRemaining;
+            timer.innerHTML = Game.gameData.timer.timeRemaining;
         }, 1000);
 
         return refreshId;
@@ -248,14 +269,6 @@ class GameUI
         questionSection.style.display = "block";
     }
 
-    static hideResultsBox() {
-        resultsSection.style.display = "none";
-    }
-
-    static showResultsBox() {
-        resultsSection.style.display = "block";
-    }
-
     static makeTimerGreen() {
         timer.style.border = `3px solid ${CONFIG.colors.green}`;
         timer.style.backgroundColor = "#f4f4f4";
@@ -272,12 +285,57 @@ class GameUI
         })
     }
 
+    static hideResultsBox() {
+        resultsSection.style.display = "none";
+    }
+
+    static showResultsBox() {
+        resultsSection.style.display = "block";
+    }
+
     static updateResultsBox(results) {
         const correctPercentage = 100 * results.correct / (results.correct + results.wrong);
-        resultsSummary.innerText = `You answered ${correctPercentage.toFixed(2)}% of the questions correct`;
-        resultsCorrects.innerText = `Correct answers: ${results.correct}`;
-        resultsWrongs.innerText = `Wrong answers: ${results.wrong}`; 
+        resultsSummary.innerHTML = `You answered ${correctPercentage.toFixed(2)}% of the questions correct`;
+        resultsCorrects.innerHTML = `Correct answers: ${results.correct}`;
+        resultsWrongs.innerHTML = `Wrong answers: ${results.wrong}`; 
     }
+
+    static createProgressBar(nrOfQuestions) {
+        progressBar.innerHTML = "";
+
+        if (nrOfQuestions < 2) {
+            return;
+        }
+        
+        for (let i = 0; i < nrOfQuestions; i++) {
+            const progressBarElement = document.createElement("div");
+            progressBarElement.className = "progress-bar-element";
+            progressBar.appendChild(progressBarElement);
+        }
+    }
+
+    /*
+        Answer is either correct or wrong
+    */
+    static updateProgressBar(index, answer) {
+        try
+        {
+            if (answer == "correct") {
+                progressBar.children[index].style.backgroundColor = "#0c0";
+            }
+            else {
+                progressBar.children[index].style.backgroundColor = "#f22";
+            }
+        }
+        catch (ex) {}
+        
+    }
+}
+
+
+class Error
+{
+
 }
 
 
@@ -298,13 +356,15 @@ class QuestionAPI
             APIUrl += `&type=${parameters["type"]}`;
         }
 
+        console.log(APIUrl);
+
         return APIUrl;
     }
 
     static getQuestions(APIUrl) {
         return fetch(APIUrl)
             .then(res => res.json())
-            .then(data => Game.init(data.results));
+            .then(data => Game.init(data))
     }
 }
 
@@ -332,4 +392,5 @@ form.addEventListener("submit", e => {
 // Click event for restarting game
 restartButton.addEventListener("click", Game.restartGame);
 
-
+// Click event for stopping game
+exitBtn.addEventListener("click", Game.restartGame);
